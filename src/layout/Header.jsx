c1f90c92a5cdd7cf8f18135fd3909d2502e64826
Fooxeek <foxeek02@gmail.com>
2023-04-04 13:17:49 +0200

    fix: Header.jsx

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/img/logo.svg";
 import Nav from "../components/Nav";
 
 export default function Header() {
 	return (
 		<header className="headerWrap">
-			<a href="/">
+			<Link to="/" className="headerWrap__fig">
 				<img className="logo" src={logo} alt="logo de l'agence kasa" />
-			</a>
+			</Link>
 			<Nav className="nav-header" />
 		</header>
 	);
