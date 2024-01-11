6c1858e959520125a040325414b7f91883f48db9
Fooxeek <foxeek02@gmail.com>
2024-01-11 13:35:22 +0400

    fix port

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,18 +5,18 @@ import { Link } from "react-router-dom";
 import logements from "../assets/datas/logements.json";
 
 export default function Home() {
-	return (
-		<>
-			<Banner />
-			<div className="cards-container">
-				{logements.map((appart, id) => (
-					<div className="card_logement" key={id}>
-						<Link className="link_card_logement" to={`/logement/${appart.id}`}>
-							<Card cover={appart.cover} title={appart.title} />
-						</Link>
-					</div>
-				))}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Banner />
+      <div className="cards-container">
+        {logements.map((appart, id) => (
+          <div className="card_logement" key={id}>
+            <Link className="link_card_logement" to={`/logement/${appart.id}`}>
+              <Card cover={appart.cover} title={appart.title} />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
